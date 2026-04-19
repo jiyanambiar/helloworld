eef7d504508dd977a2fdd079740ede7adcda727c
jiyanambiar <jiya.nambiar2024@gmail.com>
2026-04-20 01:18:01 +0530

    Update HelloApp.java

--- STEP-UC6-display-hello/Hello-App/HelloApp.java
+++ STEP-UC6-display-hello/Hello-App/HelloApp.java
@@ -1,5 +1,20 @@
-public class HelloApp{
+public class HelloSubstring {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+
+            String result = "";
+
+            for (String name : args) {
+                result += name + ", ";
+            }
+
+            // Remove last ", "
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println("Hello " + result);
+        }
     }
 }
